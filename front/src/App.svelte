f926a108d69d124ac0d0a6c2d65f1f006c5039b4
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-09-04 01:00:37 +0200

    ???

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,14 +1,12 @@
 <script>
   import Header from "./components/Header.svelte";
   import Connect from "./components/Connect.svelte";
-  const token = localStorage.getItem("token");
 </script>
 
 <main>
   <Header />
-  {#if !token}
-    <Connect />
+  {#if false}
   {:else}
-    <h1>Vous êtes connecté !</h1>
+    <Connect />
   {/if}
 </main>
--- front/src/assets/dataMapper.js
+++ front/src/assets/dataMapper.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
-
 async function login(email, password) {
     try {
         const response = await axios({
